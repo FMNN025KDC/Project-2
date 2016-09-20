ecdd8274089aa649af07afd8310d0d5ac75442b0
Attillo <david.olsson.do@gmail.com>
2016-09-20 22:39:35 +0200

    Updated function to return function object.

--- RosenbrockFunction.py
+++ RosenbrockFunction.py
@@ -6,6 +6,8 @@ Created on Tue Sep 20 20:50:05 2016
 from  scipy import *
 from  pylab import *
 
-def Rosenbrock(x):
-    x = array(x)
-    return 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2
+def Rosenbrock():
+    def RBFunc(x):
+        x = array(x)
+        return 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2
+    return RBFunc
