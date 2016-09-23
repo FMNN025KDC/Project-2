73d2e9aebb78579dfa1347659f87180a87d25095
Attillo <david.olsson.do@gmail.com>
2016-09-23 14:48:00 +0200

    See last commit to master

--- OptimizerClass.py
+++ OptimizerClass.py
@@ -10,17 +10,30 @@ class Optimizer():
     def __init__(self, function, fgradient = None):
         self.function = function
         if fgradient is None:     
-            self.fgradient = Optimizer.calculateGradient(function)
+            self.fgradient = Optimizer.calculateDifference(function)
         else:
             self.fgradient = fgradient
             
     @classmethod
-    def calculateGradient(cls,function,epsilon = 1e-8):
-        def grad(x):
-            point = array(x) +0.0
-            res = array(x) +0.0
+    def calculateDifference(cls,function,Hessian = False):
+        if Hessian:
+            function = cls.calculateDifference(function)
+        
+        def differentiate(x):
+            epsilon = 1e-8
+            if (not isinstance(x,ndarray)) and (not isinstance(x,list)): 
+                point = array([x]) +0.0
+                res = array([x]) +0.0
+            else:
+                point = array(x) +0.0
+                res = array(x) +0.0
+                
+            if Hessian:
+                epsilon = 1e-5
+                res = zeros((len(point),len(point)))
             
             for i in range(0,len(point)):
+#                print(array(point))
                 xf = array(point)
                 xf[i] = xf[i] +epsilon
                 xb = array(point)
@@ -28,5 +41,29 @@ class Optimizer():
                 res[i] = (function(xf) - function(xb)) / (2 * epsilon)
 
             return res
-        return grad
+        return differentiate
+        
+#    @classmethod
+#    def calculateHessian(cls,gfunction,epsilon = 1e-5):
+#        def hess(x):
+#            if (not isinstance(x,ndarray)) and (not isinstance(x,list)): 
+#                point = array([x]) +0.0
+#                res = array([x]) +0.0
+#            else:
+#                point = array(x) +0.0
+#                res = array(x) +0.0
+#            
+#            res = array([[0.,0.],[0.,0.]])
+#            
+#            for i in range(0,len(point)):
+##                print(array(point))
+#                xf = array(point)
+#                xf[i] = xf[i] +epsilon
+#                xb = array(point)
+#                xb[i] = xb[i] -epsilon
+#                res[i] = (gfunction(xf) - gfunction(xb)) / (2 * epsilon)
+#
+#            return res
+#        return hess
+                
                 
