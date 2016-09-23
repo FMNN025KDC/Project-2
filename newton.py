80c5d1f13473dd0fe31bd6b2a4beecdb0faea109
kristoffersvendsen <bas11ksv@student.lu.se>
2016-09-23 14:59:13 +0200

    Add files via upload
    newtons method...

--- newton.py
+++ newton.py
@@ -7,24 +7,47 @@ Created on Thu Sep 22 12:41:09 2016
 
 from  scipy import *
 from  pylab import *
+import scipy.optimize as optimize
+
 
 class Optimizer():
     def __init__(self, function, fgradient = None):
         self.function = function
         if fgradient is None:     
-            self.fgradient = Optimizer.calculateGradient(function)
+            self.fgradient = Optimizer.calculateDifference(function)
         else:
             self.fgradient = fgradient
             
             
+    def function_alpha(self,x_k,s_k):
+        
+        def func_alpha(alpha):
+            return self.function(x_k + alpha * s_k)
+            
+        return func_alpha
+
+            
             
     @classmethod
-    def calculateGradient(cls,function,epsilon = 1e-8):
-        def grad(x):
-            point = array(x) +0.0
-            res = array(x) +0.0
+    def calculateDifference(cls,function,Hessian = False):
+        if Hessian:
+            function = cls.calculateDifference(function)
+        
+        def differentiate(x):
+            epsilon = 1e-8
+            if (not isinstance(x,ndarray)) and (not isinstance(x,list)): 
+                point = array([x]) +0.0
+                res = array([x]) +0.0
+            else:
+                point = array(x) +0.0
+                res = array(x) +0.0
+                
+            if Hessian:
+                epsilon = 1e-5
+                res = zeros((len(point),len(point)))
             
             for i in range(0,len(point)):
+#                print(array(point))
                 xf = array(point)
                 xf[i] = xf[i] +epsilon
                 xb = array(point)
@@ -32,62 +55,78 @@ class Optimizer():
                 res[i] = (function(xf) - function(xb)) / (2 * epsilon)
 
             return res
-        return grad
+        return differentiate
+    
+    
+#    @classmethod
+#    def calculateHessian(cls, function,epsilon=1e-8):
+        
+        
+        
 
-
+                
 
     def newton(self, f, x,dx=0.00001):
-
+        x=array(x,dtype=float)
         alpha=1
-            
-        while True:                
-#            gx1=(f(x1+dx,x2)-f(x1-dx, x2))/(2*dx)   
-#            gx2=(f(x1,x2+dx)-f(x1, x2-dx))/(2*dx)
-#            g=array([gx1,gx2])
-            g=self.fgradient(x)       
-            H11=(f(x[0]+dx,x[1])+f(x[0]-dx, x[1])-2*f(x[0],x[1]))/(dx**2)   
-           
-           
-            H22=(f(x[0],x[1]+dx)+f(x[0], x[1]-dx)-2*f(x[0],x[1]))/(dx**2)
-            
-            
-            H12=(f(x[0]+dx,x[1]+dx)-f(x[0]+dx,x[1]-dx)-f(x[0]-dx,x[1]+dx)+f(x[0]-dx,x[1]-dx))/(4*dx**2)
-            
-            Bbar=array([[H11, H12], [H12, H22]])
+        
     
-            B=0.5*(Bbar+transpose(Bbar))
-     
-    #            print(B,x1,x2)
+        g=self.fgradient(x)  
+        
+        
+        while True:                
+            
+            gk0=g            
+            
+            g=self.fgradient(x) 
+
+            gk1=g              
+            
+#            H11=(f([x[0]+dx,x[1]])+f([x[0]-dx, x[1]])-2*f([x[0],x[1]]))/(dx**2)   
+#            H22=(f([x[0],x[1]+dx])+f([x[0], x[1]-dx])-2*f([x[0],x[1]]))/(dx**2)
+#            H12=(f([x[0]+dx,x[1]+dx])-f([x[0]+dx,x[1]-dx])-f([x[0]-dx,x[1]+dx])+f([x[0]-dx,x[1]-dx]))/(4*dx**2)
+            
+#            Bbar=array([[H11, H12], [H12, H22]])
+            
+            Gbar=Optimizer.calculateDifference(self.function, True) 
+            
+            G=0.5*(Gbar(x)+transpose(Gbar(x)))
+            
+            print(G)
+
             try:
-                L = cholesky(B)
+                L = cholesky(G)
             except LinAlgError:
                 print('matrix no psd')
+
             y=solve(L,-g)
             s=solve(L.conj().T,y)
             
- 
-                
-               
-            print(alpha)
-    #            print("s=",s)
-    #            G=inv(B)
-    #            s=-dot(G,g)
+            alpha_func=self.function_alpha(x,s)
+            alpha=optimize.minimize(alpha_func, 1)
+            alpha=alpha.x
             
-     #           print(shape(G))
-     #           print(shape(s))
-    #            print(shape(g))
-    
+            xk0=x
+            
             x[0]=x[0]+alpha*s[0]
             x[1]=x[1]+alpha*s[1]
-    
-            print('f=',f(x1,x2))
-            '''break criterion here...?'''
-    #            i+=1
-    #            j+=1
-    
-            if norm(g)< 0.000001:
-                print(x1,x2)
+            
+            xk1=x
+
+#
+#            gamma=gk1-gk0
+#            delta=xk1-xk0
+#            u=delta-H*gamma
+#            a=1/(u.T*gamma)
+#            
+#            H=H+a*u*u.T
+        
+            print('normg',norm(g))
+            if norm(g) < 1e-9:
+                
+                print('x=',x)
                 break
+
 
 def Rosenbrock():
     def RBFunc(x):
@@ -97,5 +136,6 @@ def Rosenbrock():
 
 f=Rosenbrock()    
 testopt=Optimizer(f)
-x0=array([-1,1])
+x0=array([4,-4])
 G=testopt.newton(f,x0)
+
