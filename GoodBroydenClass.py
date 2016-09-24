68bb384ef4e9cb01ed308d9c012907b5e56b59c0
kristoffersvendsen <bas11ksv@student.lu.se>
2016-09-24 11:08:41 +0200

    Add files via upload
    fixat dimensioner i goodbroyden, netwon classen har ett extra arg nu som väljer antingen classic för cholesky lösning av H elelr GB för good broyden (som inte funkar atm)

--- GoodBroydenClass.py
+++ GoodBroydenClass.py
@@ -1,7 +1,7 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 23 10:31:24 2016
-
 @author: PeterS
 """
 from scipy import *
@@ -11,23 +11,17 @@ from pylab import *
 class GoodBroyden(Optimizer):
     
     @classmethod
-    def updateHess(cls,delta,gamma,hessOld):
-        gamma = array(gamma)[None]
-        delta = array(delta)[None]
+    def updateHess(cls,delta,gam,hessOld):
+#        gam = array(gam)[None]
+#        delta = array(delta)[None]
+        print(hessOld)
         
+#        gam = gam.reshape(size(gam),1)
+#        delta = delta.reshape(size(delta),1)
         
-        gamma = gamma.reshape(size(gamma),1)
-        delta = delta.reshape(size(delta),1)
-        
-        u = array(delta - hessOld*gamma)[None]
-        a = 1/(u.T*gamma)
-        
-        H = hessOld + a*u*u.T   # behöver nog använda dot() och outer() här för multiplikationer, även ovanför. gör man det blir dimensionerna rätt men nått annat är fel med algoritmen, divergerar satan i det//kristoffer
-        
+        u = delta - dot(hessOld,gam) #1x2 
+        print('u',u)
+        a = 1/dot(u.T,gam)    #scalar
+        print('a',a)
+        H = hessOld + a*outer(u,u.T)
         return H
-        
-        
-        
-        
-    
-    
--- OptimizerClass.py
+++ OptimizerClass.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 22 11:19:40 2016
-@author:  tfy12dol
+Created on Fri Sep 23 14:59:40 2016
+@author: bas11ksv
 """
+
 from  scipy import *
 from  pylab import *
+import scipy.optimize as optimize
+
 
 class Optimizer():
-    def __init__(self, function, fgradient = None):
+    def __init__(self, function, fgradient = None, rho=0.1, sigma=0.7, tau=0.1, xi=9):
+        self.rho = rho
+        self.sigma = sigma
+        self.tau = tau
+        self.xi = xi    
         self.function = function
+        
         if fgradient is None:     
             self.fgradient = Optimizer.calculateDifference(function)
         else:
@@ -33,7 +41,6 @@ class Optimizer():
                 res = zeros((len(point),len(point)))
             
             for i in range(0,len(point)):
-#                print(array(point))
                 xf = array(point)
                 xf[i] = xf[i] +epsilon
                 xb = array(point)
@@ -41,29 +48,153 @@ class Optimizer():
                 res[i] = (function(xf) - function(xb)) / (2 * epsilon)
 
             return res
-        return differentiate
+        return differentiate     
+
+
+
+
+       
+    def function_alpha(self,x_k,s_k):
         
-#    @classmethod
-#    def calculateHessian(cls,gfunction,epsilon = 1e-5):
-#        def hess(x):
-#            if (not isinstance(x,ndarray)) and (not isinstance(x,list)): 
-#                point = array([x]) +0.0
-#                res = array([x]) +0.0
-#            else:
-#                point = array(x) +0.0
-#                res = array(x) +0.0
-#            
-#            res = array([[0.,0.],[0.,0.]])
-#            
-#            for i in range(0,len(point)):
-##                print(array(point))
-#                xf = array(point)
-#                xf[i] = xf[i] +epsilon
-#                xb = array(point)
-#                xb[i] = xb[i] -epsilon
-#                res[i] = (gfunction(xf) - gfunction(xb)) / (2 * epsilon)
-#
-#            return res
-#        return hess
+        def func_alpha(alpha):
+            return self.function(x_k + alpha * s_k)
+            
+        return func_alpha
+
+
+        
+    def inexactLineSearch(self,x_k,s_k):
+        alpha_U = 1e99
+        alpha_L = 0
+        alpha_0 = 1
+        
+        func_alpha = self.function_alpha(x_k,s_k)
+        grad_alpha = self.calculateDifference(func_alpha)
+
+        LC,RC = self.goldsteinCondition(alpha_0,alpha_U,alpha_L,func_alpha,grad_alpha)
+        
+        while (not (LC and RC)):
+            if (not LC):
+#                print("LC")
+                Dalpha_0 = (alpha_0 - alpha_L) * \
+                    grad_alpha(alpha_0) / (grad_alpha(alpha_L) - grad_alpha(alpha_0))
+                Dalpha_0 = max(Dalpha_0[0], self.tau * (alpha_0 - alpha_L))
+                Dalpha_0 = min(Dalpha_0, self.xi * (alpha_0 - alpha_L))
+                alpha_L = alpha_0
+                alpha_0 = alpha_0 + Dalpha_0
+            else:
+#                print("RC")
+                alpha_U = min(alpha_0, alpha_U)
+#                print("Alpha:")
+#                print(alpha_0)
+                Balpha_0 = (alpha_0 - alpha_L)**2 * grad_alpha(alpha_0) / \
+                    (2 * (func_alpha(alpha_L) - func_alpha(alpha_0) + (alpha_0 - alpha_L) * grad_alpha(alpha_L)))
+                Balpha_0 = max(Balpha_0[0], alpha_L + self.tau * (alpha_U - alpha_L))
+                Balpha_0 = min(Balpha_0, alpha_U - self.tau * (alpha_U - alpha_L))
+                alpha_0 = Balpha_0
+            
+            LC,RC = self.goldsteinCondition(alpha_0,alpha_U,alpha_L,func_alpha,grad_alpha)
+            
+        return alpha_0
+
+    def exactLineSearch(self,x_k,s_k):
+        print('x_k',x_k)
+        print('s_k',s_k)        
+        alpha=1
+        alpha_func=self.function_alpha(x_k,s_k)
+        alpha=optimize.minimize(alpha_func, alpha)
+        alpha=alpha.x
+        return alpha
+           
+    def goldsteinCondition(self,alpha_0,alpha_U,alpha_L,func_alpha,grad_alpha):
+        LC = (func_alpha(alpha_0) >= func_alpha(alpha_L) + \
+            (1 - self.rho) * (alpha_0 - alpha_L) * grad_alpha(alpha_L))
+        RC = (func_alpha(alpha_0) <= func_alpha(alpha_L) + \
+            self.rho*(alpha_0 - alpha_L)*grad_alpha(alpha_L))
+            
+        return (LC[0],RC[0])
+            
+            
+        
+    def newton(self, x, Inexact=True, method='classic'):
+        x=array(x,dtype=float)          
+        g=self.fgradient(x)
+
+        run_once=True
+        while True:
+                            
+            
+            gk0=g            
+            
+            g=self.fgradient(x) 
+    
+            gk1=g            
+          
+            Gbar=Optimizer.calculateDifference(self.function, True)             
+            G=0.5*(Gbar(x)+transpose(Gbar(x)))
+            
+            if method=='classic':
+            
+                try:
+                    L = cholesky(G)
+                except LinAlgError:
+                    print('matrix no psd')
+                    break
                 
+                y=solve(L,-g)
+                s=solve(L.conj().T,y)
                 
+            elif method =='GB':
+                if run_once==True:
+                    Gbar=Optimizer.calculateDifference(self.function, True)             
+                    G=0.5*(Gbar(x)+transpose(Gbar(x)))
+                    H=inv(G)
+
+                    s=-dot(H,g)
+                    print('s',s)
+                    xk0=array(x)
+                    print('xk0',xk0)
+                    for i in range(0, len(x)):
+                        x[i]=x[i]+1*s[i]
+                    xk1=array(x)
+                    g=self.fgradient(x)    
+                    gk1=g       
+                    print('xk1',xk1)                                      
+                    run_once=False
+                    
+                gamma=gk1-gk0
+                print('gamma',gamma)                
+                delta=xk1-xk0
+                print('delta',delta)
+                H = GoodBroyden.updateHess(delta,gamma,H)
+                print('H',H,'g',g)
+                print(shape(H),type(H))
+                s=-dot(H,g)
+                print('s',s)
+                print('x',x)
+                
+                    
+            if Inexact:
+                alpha=self.inexactLineSearch(x,s)
+            else:
+                alpha=self.exactLineSearch(x,s)
+            
+            xk0=x
+
+            for i in range(0, len(x)):
+                x[i]=x[i]+alpha*s[i]
+            
+            xk1=x
+            
+            
+            
+
+            
+        
+            
+            
+            print(x,alpha)
+            print('normg',norm(g))
+            if norm(g) < 1e-5:
+                print('x=',x)
+                break
